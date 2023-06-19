405aff9f05a263de491a6886b9d0758d9b281022
Luísa Lima <luisalima0602@gmail.com>
2023-06-18 23:03:22 -0300

    Atividade 2 da disciplina

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -1,33 +1,49 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+const AppNavBar = () =>{
+  return(
+    <div>
+      <h1>Fazendinha</h1>
+    </div>
+  )
+}
+
+const AppEntrada = (props: any) => {
+  
+  return(
+    <div className="card">
+      <label htmlFor='nome' className="form-label">Nome:</label>
+      <br/>
+      <input 
+        type="texto"
+        id='nome'
+        className='effect-16'
+        placeholder='Informe o nome do animal'
+        value={props.texto} 
+        onChange={props.mudarTexto}/>
+       
+    </div>
+  )
+}
+
+const AppCloneEntrada = (props: any) =>{
+  return(
+    <label className='fixed-label'>{props.texto}</label>
+  )
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [entrada, setEntrada] = useState ("")
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <AppNavBar/>
+      <br/>
+      <AppEntrada texto={entrada} mudarTexto={(e: any) => setEntrada(e.target.value)}/>
+      <br/>
+      <AppCloneEntrada texto = {entrada}/>
     </>
   )
 }
